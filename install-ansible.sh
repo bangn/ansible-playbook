8a6a6e85cc8913c17d6a6c7174c5d514dc8e9e2e
bangn <thebangnguyen@gmail.com>
2019-08-17 16:33:15 +1000

    Install ansible for debian

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,5 +1,4 @@
-sudo apt-get update
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo echo deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main | sudo tee -a /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 sudo apt-get update
 sudo apt-get install ansible -y
