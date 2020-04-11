b660c83dc2bed55fe8809ab833cd981a62179b31
bangn <thebangnguyen@gmail.com>
2020-04-11 18:50:06 +1000

    Move script to install ansible to bootstrap script

--- install-ansible.sh
+++
@@ -1,4 +0,0 @@
-sudo echo deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main | sudo tee -a /etc/apt/sources.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-sudo apt-get update
-sudo apt-get install ansible -y
